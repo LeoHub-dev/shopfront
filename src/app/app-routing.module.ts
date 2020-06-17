568f3fafb7bc24c0efe063e41c55cc622ffb47c4
Leonardo JImenez <jimenezleonardop@gmail.com>
2020-06-16 23:22:20 -0500

    Init app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListComponent } from './list/list.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListComponent },
+  { path: 'checkout', component: CheckoutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
